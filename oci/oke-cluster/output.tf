dbdaedbcbad9fe07af3153473d3a8d141ee9f8c4
Cleverson Rodrigues <thefly72003@yahoo.com.br>
2023-09-22 13:35:35 -0300

    first commit

---
+++ oci/oke-cluster/output.tf
@@ -0,0 +1,23 @@
+
+############ cluster id ############
+output "cluster_id" {
+  description = "ID of the Kubernetes cluster"
+  value       = oci_containerengine_cluster.generated_oci_containerengine_cluster.id
+}
+
+############ node pool id ############
+output "nodepool_ids" {
+  description = "Map of Nodepool names and IDs"
+  value       = oci_containerengine_node_pool.create_node_pool_details0.id
+}
+
+############ compute instances ############
+output "selfhosted_instance_public_ip" {
+  description = "Public IP address of the selfhosted_instance"
+  value       = oci_core_instance.selfhosted_instance.public_ip
+}
+
+output "vpn_instance_public_ip" {
+  description = "Public IP address of the vpn_instance"
+  value       = oci_core_instance.vpn_instance.public_ip
+}
