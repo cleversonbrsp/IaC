05a640bb7982f21003bed5250493cb531eeb7199
Cleverson <68443857+cleversonbrsp@users.noreply.github.com>
2023-12-19 18:01:07 -0300

    Delete oci/oke-cluster-tf directory

--- oci/oke-cluster-tf/compartments.tf
+++
@@ -1,6 +0,0 @@
-resource "oci_identity_compartment" "oke_comp" {
-  compartment_id = var.oci_root_compartment
-  description    = "OKE Homolog - terraform environment."
-  name           = "OKE_HML"
-  enable_delete  = true
-}
--- oci/oke-cluster-tf/output.tf
+++
@@ -1,9 +0,0 @@
-output "cluster_id" {
-  description = "ID of the Kubernetes cluster"
-  value       = oci_containerengine_cluster.generated_oci_containerengine_cluster.id
-}
-
-output "nodepool_ids" {
-  description = "Map of Nodepool names and IDs"
-  value       = oci_containerengine_node_pool.create_node_pool_details0.id
-}
