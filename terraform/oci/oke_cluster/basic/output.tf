883565505b60dc5194d49b03ad655c4427fec2a4
Cleverson Rodrigues <thefly72003@yahoo.com.br>
2024-05-11 17:15:45 -0300

    atz

--- terraform/oci/oke_cluster/basic/output.tf
+++ terraform/oci/oke_cluster/basic/output.tf
@@ -8,6 +8,3 @@ output "nodepool_ids" {
   value       = oci_containerengine_node_pool.node_pool01.id
 }
 
-output "oke_context" {
-  value = oci_container_engine_cluster.generated_oci_containerengine_cluster.context
-}
