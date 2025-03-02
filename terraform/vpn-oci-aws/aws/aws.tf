14ef1290f78abf868a7278643fb4d2f43aaf69b8
Cleverson Rodrigues <cleverson.ccna@gmail.com>
2025-03-02 07:35:05 -0300

    new tf files

--- terraform/vpn-oci-aws/aws/aws.tf
+++ terraform/vpn-oci-aws/aws/aws.tf
@@ -2,72 +2,56 @@ provider "aws" {
   region = "us-east-1"
 }
 
-# # VPC
-# resource "aws_vpc" "example_vpc" {
-#   cidr_block = "10.0.0.0/16"
-#   enable_dns_support = true
-#   enable_dns_hostnames = true
-
-#   tags = {
-#     Name = "example_vpc"
-#   }
-# }
-
-# Gateway Virtual Privado
-resource "aws_vpn_gateway" "example_vpn_gateway" {
-  vpc_id = "vpc-0c003607e3fe9a2b0"
+# VPC
+resource "aws_vpc" "vpc_crs" {
+  cidr_block = "10.0.0.0/16"
+  enable_dns_support = true
+  enable_dns_hostnames = true
 
   tags = {
-    Name = "gtw-itau-isolado"
+    Name = "vpc_crs"
   }
-
-#  depends_on = [aws_vpc.example_vpc] # Garantir que a VPC seja criada primeiro
 }
 
-# Gateway Client Temporário
+# Temporary Customer Gateway
 resource "aws_customer_gateway" "temp_gateway" {
-  bgp_asn    = 65000
-  ip_address = "203.0.113.1" # IP público temporário
+  bgp_asn    = 31898
+  ip_address = "1.1.1.1"
   type       = "ipsec.1"
 
   tags = {
-    Name = "gtw-temp-itau-isolado"
+    Name = "TempGateway"
   }
 }
 
-# Conexão VPN com o Gateway Temporário
-resource "aws_vpn_connection" "temp_vpn_connection" {
-  vpn_gateway_id      = aws_vpn_gateway.example_vpn_gateway.id
+# Virtual Private Gateway (VPG)
+resource "aws_vpn_gateway" "vpg" {
+  amazon_side_asn = "64512"  # Default Amazon ASN
+
+  tags = {
+    Name = "crs-vpg"
+  }
+}
+
+# Attach Virtual Private Gateway to VPC
+resource "aws_vpn_gateway_attachment" "vpg_attachment" {
+  vpc_id          = aws_vpc.vpc_crs.id
+  vpn_gateway_id  = aws_vpn_gateway.vpg.id
+
+  depends_on = [aws_vpn_gateway.vpg]
+}
+
+# VPN Connection
+resource "aws_vpn_connection" "vpn_connection" {
+  vpn_gateway_id      = aws_vpn_gateway.vpg.id
   customer_gateway_id = aws_customer_gateway.temp_gateway.id
   type                = "ipsec.1"
-  static_routes_only  = true
-
-  depends_on = [
-    aws_vpn_gateway.example_vpn_gateway, 
-    aws_customer_gateway.temp_gateway
-  ] # Garantir que o gateway e o cliente estejam criados
-}
-
-# Gateway Client Final
-resource "aws_customer_gateway" "final_gateway" {
-  bgp_asn    = 65001
-  ip_address = "198.51.100.1" # IP público real do ponto final da VPN do OCI
-  type       = "ipsec.1"
+  tunnel1_inside_cidr = "169.254.20.0/30"  # Ensure this CIDR is allowed by OCI
+  tunnel1_preshared_key = "Xy7pLm9Qz42sD"
 
   tags = {
-    Name = "final-gtw-itau-isolado"
+    Name = "crs-vpn-connection"
   }
-}
 
-# Conexão VPN com o Gateway final (opcional)
-resource "aws_vpn_connection" "final_vpn_connection" {
-  vpn_gateway_id      = aws_vpn_gateway.example_vpn_gateway.id
-  customer_gateway_id = aws_customer_gateway.final_gateway.id
-  type                = "ipsec.1"
-  static_routes_only  = true
-
-  depends_on = [
-    aws_customer_gateway.final_gateway, 
-    aws_vpn_gateway.example_vpn_gateway
-  ] # Garantir que os gateways estejam criados
+  depends_on = [aws_vpn_gateway_attachment.vpg_attachment]
 }
